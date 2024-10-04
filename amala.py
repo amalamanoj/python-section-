5e7eb68f33d2126a0d2f0f13451327e7d8332cd2
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:38 +0530

    Add files via upload

---
+++ amala.py
@@ -0,0 +1,43 @@
+#print("hello\nworld")
+#print("Five is greater than two")
+'''if 5 > 2:
+   print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two")
+x=5
+x="ponnu"
+print(x)
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+x = 5
+y = "amalu"
+print(type(x))
+print(type(y))
+x = 1j
+print(type(x))
+x = 1.6
+print(type(x))
+my_name = "amalu"
+print(my_name)
+x, y, z = "Orange", "Banana","Cherry"
+print(x)
+print(y)
+print(z)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x + y + z)
+username = input("Enter username:")
+print("Username is : " + username)
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+'''
+a = "Hello, World"
+print(a[:12])
