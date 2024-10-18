94b25b47f6d65283cd37b2a0f231d74f28b851b7
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-18 15:10:52 +0530

    Create pyton2.py

---
+++ pyton2.py
@@ -0,0 +1,11 @@
+'''
+Author:Amala Manoj
+Date:18-10-2024
+Description:a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+
+str_var="Hello, World!"
+str_another_var=str_var[7:12]
+print(str_another_var)
+str_var2=" Everyone!"
+str_var3= str_another_var+str_var2
+print(str_var3)
