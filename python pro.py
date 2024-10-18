a25b198ba0cab9481bcbea410e125f581cad122f
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-18 16:19:17 +0530

    Create python pro.py

---
+++ python pro.py
@@ -0,0 +1,12 @@
+''
+Author: Amala Manoj
+Date:18-10-2024
+Description:a Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+'''import  math
+number=int(input("Enter a number:"))
+square_root=math.sqrt(number)
+print("Square root of ",number,":",square_root)
+factorial=math.factorial(number)
+print("factorial of",number,":",factorial)
+power=math.pow(number,2)
+print("raise the number of the power of 2",number,":",power)
