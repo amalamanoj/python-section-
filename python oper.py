fcf66619e8df262b7eb98b993a09c7eb995a6951
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-18 16:26:31 +0530

    Create python oper.py

---
+++ python oper.py
@@ -0,0 +1,7 @@
+'''
+Author:Amala Manoj
+Date:18-10-2024
+Descrition:Python program that demonstrates the usage of arithmetic, comparison
+'''number1=int(input("Enter your first number "))
+number2=int(input("Enter your second number"))
+print("sum=",number1+number2,"Division=",number1/number2)
