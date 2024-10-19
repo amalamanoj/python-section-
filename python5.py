c58fec2bb8992852504fccf7fafb9425420f0c28
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-19 15:52:25 +0530

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,11 @@
+iv=num1/num2
+print("The quotient when num1 is divided num2 is:",div)num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3:"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+Difference=num2-num1
+print("The difference when num2 is subtracted from num1 is:",Difference)
+multi_result=num1*num2
+print("The product of num1 and num2 is:",multi_result)
+d
