a816aa3116bb79f747fd35e481958a375e841384
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-10-25 14:38:49 +0530

    Add files via upload

---
+++ python program2.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+if num2>0:
+    print(num2 ,"is a positive number")
+elif num2<0:
+    print(num2," is a negative number")
+else:
+    print(num2,"is a zero")
