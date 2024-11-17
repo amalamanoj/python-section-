1bcc2a0aeb1fe73f96fee0d1b57c306ab9105594
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-17 12:47:33 +0530

    Create if and else.py

---
+++ if and else.py
@@ -0,0 +1,13 @@
+'''
+Author:Amala Manoj
+Date:17-11-2024
+'''
+age=int(input("Enter your age:"))
+if(age<=13):
+    print("you are a child")
+elif(age>=13 and age<=19):
+     print("you are a teen")
+elif(age>=20 and age<=64):
+     print("you are an adult")
+else:
+     print("you are a senior")
