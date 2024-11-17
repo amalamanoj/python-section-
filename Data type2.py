b32bc680800f784e85ac12f6a1b2196bf4bd990f
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-17 12:27:52 +0530

    Create Data type2.py

---
+++ Data type2.py
@@ -0,0 +1,10 @@
+'''
+Author:Amala Manoj
+Date:17-11-2024
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age (in years):"))
+age_in_month=(age*12)
+print("Hello",name,"!")
+print("you are",age,"years old,which is",age_in_month,"months")
+
