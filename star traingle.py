cfaca84b7c1fe91b9f3e3db364164bee99b25906
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-22 16:17:30 +0530

    Update star traingle.py

--- star traingle.py
+++ star traingle.py
@@ -3,6 +3,7 @@ Author:Amala Manoj
 Date:22-11-2024
 '''
 n=int(input("Enter a num:"))
+print("Increasing Triangle")
 for i in range(1,n+1):
     for j in range(1,i):
         print("*",end=" ")
@@ -11,6 +12,7 @@ for i in range(1,n+1):
 
 
 n1=int(input("Enter a num:"))
+print("Decreasing Traingle")
 for i in range(n1):
     for j in range(n-i-1):
         print("*",end=" ")
@@ -18,6 +20,7 @@ for i in range(n1):
 
 
 row3=int(input("Enter the no of rows:"))
+print("Hill pattern")
 for i in range(row3):
     for m in range(i,row3+1):
         print(" ", end=" ")
@@ -26,6 +29,7 @@ for i in range(row3):
     print("")
 
 row4=int(input("Enter the no of rows:"))
+print("Reverse hill pattern")
 for i in range(row4,0,-1):
     for m in range(row4-i,0,-1):
         print(" ", end=" ")
