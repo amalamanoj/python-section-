92d06d4e5687f35e11175c8fc5555b67c1c6a14e
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-29 14:59:35 +0530

    Create Rigth triangle.py

---
+++ Rigth triangle.py
@@ -0,0 +1,21 @@
+'''
+Author:Amala Manoj
+Date:29-11-2024
+'''
+def triangle_side(list):
+
+    if side==side_:
+      print("The given triangle is a rigth triangle")
+    else:
+       print("It is not a triangle")
+
+
+side1=int(input("Enter the length of the first side:"))
+side2=int(input("Enter the length of the second side:"))
+side3=int(input("Enter the length of the third side:"))
+list=[side1,side2,side3]
+list.sort()
+print(list)
+side=side3*side3
+side_=side1*side1+side2*side2
+triangle_side(list)
