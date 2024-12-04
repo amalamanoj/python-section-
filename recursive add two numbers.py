417f862239ee0ddc2b0c1a53dba4fd3f9b90ccfc
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-12-04 21:50:25 +0530

    Update recursive add two numbers.py

--- recursive add two numbers.py
+++ recursive add two numbers.py
@@ -9,4 +9,4 @@ def recursive_add(num1,num2):
         return recursive_add(num1+1,num2-1)
 num1=int(input("Enter a number:"))
 num2=int(input("Enter a number:"))
-print("The sum of the two numbers is",recursive_add(2,3))
+print("The sum of the two numbers is",recursive_add(num1,num2))
